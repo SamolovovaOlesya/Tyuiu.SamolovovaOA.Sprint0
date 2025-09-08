614faaa46f13dd18a8b0ffe457831f37d6251ac3
Olesya Samolovova <Tyuiu.SamolovovaOA@outlook.com>
2025-09-08 18:53:48 +0500

    Task2

---
+++ Tyuiu.SamolovovaOA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SamolovovaOA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.SamolovovaOA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Олеся";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Олеся", res);
+        }
+    }
+}
